8d9bf08b7c1047415328d37322bb6d28b1009d4b
Sulaiman M <sulaiman@host-161-23-168-67.qmul.ac.uk>
2018-11-08 15:03:45 +0000

    Created Unity-2D folder which will contain all of the 2D projects I will be creating in Unity.

---
+++ Unity2D/intro-setup/Number Wizard Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int maxValue = 1000;
+        int lowestValue = 1;
+        int guess = 0;
+
+        Debug.Log("Welcome to number wizard, yo");
+        Debug.Log("Please think of a number.");
+        Debug.Log("highest number you can pick is: " + maxValue);
+        Debug.Log("lowest number you can pick is: " + lowestValue);
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
+        Debug.Log("Please use your arrow keys, Push-up = Higher, Push-down = Lower, Enter = Correct");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+           Debug.Log("Up arrow has been pressed.");
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)){
+            Debug.Log("Down arrow has been pressed.");
+        }else if (Input.GetKeyDown(KeyCode.Return)) {
+            Debug.Log("Enter key has been pressed.");
+        }
+
+	}
+}
