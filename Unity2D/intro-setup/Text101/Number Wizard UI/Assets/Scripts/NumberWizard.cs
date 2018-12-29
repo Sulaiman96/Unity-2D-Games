8055dd90a9bf9755340a10070818296ac3f3dd0e
Sulaiman M <sulaiman@Sulaimans-MacBook-Pro.local>
2018-12-29 13:49:47 +0000

    finished off a number guessing game, learnt scene transition, random number generator and how to use buttons to allow us to select what we want to happen upon clicking

---
+++ Unity2D/intro-setup/Text101/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+
+    [SerializeField] int maxValue;
+    [SerializeField] int lowestValue;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame(){
+        NextGuess();
+    }
+
+    public void OnPressHigher(){
+        lowestValue = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower(){
+        maxValue = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess(){
+        guess = Random.Range(lowestValue, maxValue +1);
+        guessText.text = guess.ToString();
+    }
+
+}
