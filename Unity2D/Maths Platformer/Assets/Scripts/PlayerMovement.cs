9ec44a98ba7084eb8694fb24f49b7b4b72baf355
Sulaiman M <sulaiman@Sulaimans-MacBook-Pro.local>
2019-01-05 21:37:30 +0000

    Added player movement

---
+++ Unity2D/Maths Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] CharacterController2D controller;
+    float horizontalMove = 0f;
+    [SerializeField] float runSpeed = 1f;
+	// Update is called once per frame
+	void Update () {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed; //gives a value of -1 to 1 depending on which key is pressed. 
+	}
+
+    private void FixedUpdate()
+    {
+        //Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false); //Time.fixedDeltaTime makes sure our character movement speed is consistent. 
+    }
+}
