8fb498b08290fdb1660fe6bd77ede0416664e2d3
Sulaiman M <sulaiman@sulaimans-mbp.wkstn.qmul.ac.uk>
2019-01-07 13:45:42 +0000

    Added animation to character movement

---
+++ Unity2D/Maths Platformer/Assets/Scripts/Demo_GM.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class Demo_GM : MonoBehaviour {
+
+
+
+    public static Demo_GM Gm;
+
+    public Image[] UIImage;
+
+    // Use this for initialization
+    void Awake () {
+        Screen.fullScreen = false;
+        Gm = this; // assign object. 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        KeyUPDownchange(); // check to see which key is pressed.
+    }
+
+
+    void InitColor()
+    {
+
+        for (int i = 0; i < UIImage.Length; i++)
+        {
+            UIImage[i].color = new Color(255, 255, 255);
+
+
+        }
+
+    }
+
+    public void KeyUPDownchange()
+    {
+        // wsad
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Color myColor = new Color32(255, 255, 255, 255);
+
+            Demo_GM.Gm.UIImage[2].color = myColor;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Color myColor = new Color32(255, 255, 255, 255);
+
+            Demo_GM.Gm.UIImage[3].color = myColor;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Color myColor = new Color32(255, 255, 255, 255);
+
+            Demo_GM.Gm.UIImage[0].color = myColor;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            Color myColor = new Color32(255, 255, 255, 255);
+
+            Demo_GM.Gm.UIImage[1].color = myColor;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Color myColor = new Color32(180, 180, 180, 255);
+
+            Demo_GM.Gm.UIImage[2].color = myColor;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Color myColor = new Color32(180, 180, 180, 255);
+
+            Demo_GM.Gm.UIImage[3].color = myColor;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Color myColor = new Color32(180, 180, 180, 255);
+
+            Demo_GM.Gm.UIImage[0].color = myColor;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Color myColor = new Color32(180, 180, 180, 255);
+
+            Demo_GM.Gm.UIImage[1].color = myColor;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Color myColor = new Color32(180, 180, 180, 255);
+
+            Demo_GM.Gm.UIImage[4].color = myColor;
+        }
+
+
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+
+            Color myColor = new Color32(255, 255, 255, 255);
+
+            Demo_GM.Gm.UIImage[4].color = myColor;
+        }
+
+
+    }
+
+}
---
+++ Unity2D/Maths Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 20f;
+
+    //movement variables
+    private float moveInput;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    private bool facingRight = true;
+
+    //jumping variables
+    public float jumpForce = 20f;
+    private bool isGrounded;
+    public Transform groundCheck;
+    private int extraJump;
+    public int extraJumpsValue;
+    private float distance = 2f;
+
+    //animation variable
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        extraJump = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isGrounded == true){ //everytime the player touches the ground, his number of jumps is reset.
+            extraJump = extraJumpsValue;
+            OnLandEvent();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && extraJump > 0) { //checks for w or spacebar and how many jumps.
+            animator.SetBool("isGround", false);
+            rb.velocity = Vector2.up * jumpForce;
+            extraJump--;
+        }else if(Input.GetKeyUp(KeyCode.Space) && extraJump == 0 && isGrounded == true){
+            animator.SetBool("isGround", false);
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+       //Debug.DrawLine(groundCheck.position, groundCheck.position + (groundCheck.right * distance), Color.green);
+	}
+
+    private void FixedUpdate(){
+        isGrounded = Physics2D.Raycast(groundCheck.position, groundCheck.right, distance);
+        moveInput = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));  //sets the animator variables for run and jump so the correct animations play.
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);  //moves player horizontally. 
+
+        if(facingRight == false && moveInput > 0){  //check to see which direction the character is facing and moving.
+            Flip(); 
+        }else if(facingRight == true && moveInput < 0){
+            Flip();
+        }
+    }
+
+    public void OnLandEvent(){
+        animator.SetBool("isGround", true);
+    }
+
+    //This method keeps the scale (size) of the character and multiplies the x co-ordinates by -1 which flips the character. 
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
