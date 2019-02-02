f779233fdc076ac303a45aa42dd7570972e912c4
Sulaiman M <sulaiman@sulaimans-mbp.wkstn.qmul.ac.uk>
2019-02-02 16:29:02 +0000

    Added Quiz to the game, everytime the player picks up the coin, the question is presented to player and an object now keeps track of the score of the player as well

---
+++ Unity2D/Maths Platformer/Assets/Scripts/DataController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataController : MonoBehaviour
+{
+    [SerializeField] RoundData[] allRoundData; //can extend for multiple rounds.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public RoundData GetCurrentRoundData() {
+        return allRoundData[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
